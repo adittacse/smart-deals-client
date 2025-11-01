c09e4300cffee51d4665eed04ddc1db6721abd75
Aditta Chakraborty <cseaditta@gmail.com>
2025-11-02 04:24:35 +0600

    bid delete funcion implemented

--- src/components/MyBids/MyBids.jsx
+++ src/components/MyBids/MyBids.jsx
@@ -1,9 +1,117 @@
-import React from 'react';
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "../../contexts/AuthContext.jsx";
+import Swal from "sweetalert2";
 
 const MyBids = () => {
+    const [myBids, setMyBids] = useState([]);
+    const { user } = useContext(AuthContext);
+
+    useEffect(() => {
+        if (user?.email) {
+            fetch(`http://localhost:3000/my-bids?email=${user.email}`)
+                .then(res => res.json())
+                .then(data => {
+                    setMyBids(data);
+                });
+        }
+    }, [user?.email]);
+
+    const handleDeleteBid = (_id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:3000/bids/${_id}`, {
+                    method: "DELETE"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your bid has been deleted.",
+                                icon: "success"
+                            });
+                            // remaining bids
+                            const remainingBids = myBids.filter(bid => bid._id !== _id);
+                            setMyBids(remainingBids);
+                        }
+                    });
+            }
+        });
+    }
+
     return (
         <div>
-            <h2>My Bids</h2>
+            <h2 className="text-center text-5xl font-bold mt-20">My Bids: <span className="primary-text">{myBids.length}</span></h2>
+            <div className="overflow-x-auto mt-10 mb-20">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                    <tr>
+                        <th>SL No.</th>
+                        <th>Product</th>
+                        <th>Bider</th>
+                        <th>Bid Price</th>
+                        <th>Status</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        myBids.map((bid, index) => <tr key={bid._id}>
+                            <th>{index+1}</th>
+                            <td>
+                                <div className="flex items-center gap-3">
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle h-12 w-12">
+                                            <img
+                                                src={bid?.product_image}
+                                                alt="product image"/>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="font-bold">{bid?.product_title}</div>
+                                        <div className="text-sm opacity-50">${bid?.product_price_min}-{bid?.product_price_max}</div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="flex items-center gap-3">
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle h-12 w-12">
+                                            <img
+                                                src={bid?.buyer_image}
+                                                alt="product image"/>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="font-bold">{bid?.buyer_name}</div>
+                                        <div className="text-sm opacity-50">{bid?.buyer_email}</div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>${bid?.bid_price}</td>
+                            <td>
+                                {
+                                    bid?.status === "pending" ? <div className="badge badge-warning">Pending</div>
+                                        : <div className="badge badge-success">Accepted</div>
+                                }
+                            </td>
+                            <th>
+                                <button onClick={() => handleDeleteBid(bid._id)} className="btn btn-outline btn-error">Remove Bid</button>
+                            </th>
+                        </tr>)
+                    }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
