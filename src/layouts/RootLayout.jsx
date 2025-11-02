3eb520a9510e2387fdec92e792c3b5df21fbf1c0
Aditta Chakraborty <cseaditta@gmail.com>
2025-11-03 04:57:14 +0600

    layout updated and footer added

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,12 +1,18 @@
 import Navbar from "../components/Navbar/Navbar.jsx";
 import { Outlet } from "react-router";
+import Footer from "../components/Footer/Footer.jsx";
 
 const RootLayout = () => {
     return (
-        <div className="bg-[#AAACBB15]">
-            <div className="font-inter max-w-7xl mx-auto">
+        <div className="font-inter bg-[#AAACBB15]">
+            <div className="bg-base-100 shadow-sm">
                 <Navbar />
+            </div>
+            <div className="max-w-7xl mx-auto">
                 <Outlet />
+            </div>
+            <div className="bg-secondary">
+                <Footer />
             </div>
         </div>
     );
